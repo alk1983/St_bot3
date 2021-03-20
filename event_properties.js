73bb6e9a3ed1f36db2ec2b21b7d42e4e034ebeec
alk1983 <oleg_karpatschew@tut.by>
2021-03-20 16:08:50 +0300

    yandex

---
+++ event_properties.js
@@ -0,0 +1,52 @@
+ymaps.ready(init);
+var myMap;
+<?php
+if(isset($_GET['lan']) AND isset($_GET['long'])) {
+    $lan = $_GET['lan'];
+	$long = $_GET['long'];
+}
+?>
+
+function init () {
+    myMap = new ymaps.Map("map", {
+        center: ['<?php echo$lan;?>', '<?php echo$long;?>'], 
+        zoom: 11
+    }, {
+        balloonMaxWidth: 200,
+        searchControlProvider: 'yandex#search'
+    });
+
+    // Обработка события, возникающего при щелчке
+    // левой кнопкой мыши в любой точке карты.
+    // При возникновении такого события откроем балун.
+    myMap.events.add('click', function (e) {
+        if (!myMap.balloon.isOpen()) {
+            var coords = e.get('coords');
+            myMap.balloon.open(coords, {
+                contentHeader:'Событие!',
+                contentBody:'<p>Кто-то щелкнул по карте.</p>' +
+                    '<p>Координаты щелчка: ' + [
+                    coords[0].toPrecision(6),
+                    coords[1].toPrecision(6)
+                    ].join(', ') + '</p>',
+                contentFooter:'<sup>Щелкните еще раз</sup>'
+            });
+        }
+        else {
+            myMap.balloon.close();
+        }
+    });
+
+    // Обработка события, возникающего при щелчке
+    // правой кнопки мыши в любой точке карты.
+    // При возникновении такого события покажем всплывающую подсказку
+    // в точке щелчка.
+    myMap.events.add('contextmenu', function (e) {
+        myMap.hint.open(e.get('coords'), 'Кто-то щелкнул правой кнопкой');
+    });
+    
+    // Скрываем хинт при открытии балуна.
+    myMap.events.add('balloonopen', function (e) {
+        myMap.hint.close();
+    });
+}
